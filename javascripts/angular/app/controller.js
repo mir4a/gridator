810ebdcd8189d79f3ffc41b9203dced4f7508ada
Artur <coderxlsn@hotmail.com>
2013-09-29 02:40:13 +0300

    #2 выделение ячейки

--- javascripts/angular/app/controller.js
+++ javascripts/angular/app/controller.js
@@ -10,7 +10,7 @@ function MyCtrl ($scope) {
  		var cell =new Object;
  		for(var i=0;i<cells;i++)
 		{
-			cell['elemnt'+i] = {'cell':1};
+			cell['elemnt'+i] = {'cell':1,'check':0,'class1':'not-checked','index':i};
 		}
 		result.colums['row'+j] = {'row' :j,'cell':cell};
   		
@@ -18,16 +18,41 @@ function MyCtrl ($scope) {
 	console.log(result);
 	$scope.rows = result;
 	
-	$scope.$watch( function(){
+	$scope.$watch( 'rows', function(){
  	
 	
       $scope.MyStyle = {'border':'1px solid #000','height':'20px'};
     });
-    var select = {cell:null,row:null};
-    $scope.selectrow = function($index,row)
+    var select = new Object;
+    $scope.selectrow = function(cell,row)
     {	
-    	console.log($index);
-    	console.log(row);
+    	
+    	 var active = $scope.rows.colums['row'+row].cell['elemnt'+cell];
+    	 if(active.check == 0)
+    	 {
+    	 	active.class1 = 'checked';
+    	 	active.check = 1;
+    	 	$scope.select = 1;
+    	 }else{
+    	 	active.class1 = 'not-checked';
+    	 	active.check = 0;
+    	 }
+    	 select[active.$$hashKey]=1;
+    	 /*
+    	 if (select.row == null) {
+    	 	select.row = row;
+    	 	select.startcell = cell;
+    	 }else {
+    	 	if (select.startcell > cell) {
+    	 		select.colums = select.colums+( select.startcell-cell);
+    	 		select.startcell
+    	 	}else {
+    	 		select.colums = select.colums+( cell- select.startcell);
+    	 	}
+    	 }
+    	 */
+    	 console.log(select);
+    	
     }
 	 
 }
