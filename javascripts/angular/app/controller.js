d0750ce5c7bf7ba57438874cf5ddcc373a7df9fb
Artur <coderxlsn@hotmail.com>
2013-09-29 00:45:01 +0300

    #2 создание объекта

--- javascripts/angular/app/controller.js
+++ javascripts/angular/app/controller.js
@@ -1,27 +1,33 @@
 function MyCtrl ($scope) {
+
 	$scope.row = 3;
 	$scope.cell = 12;
+	var rows = $scope.row;   //СТРОКИ
+ 	var cells = $scope.cell; //КОЛОНКИ
+ 	var result = new Object;
+ 	result.colums = new Object;
+ 	for(var j=0;j<rows;j++) {
+ 		var cell =new Object;
+ 		for(var i=0;i<cells;i++)
+		{
+			cell['elemnt'+i] = {'cell':1};
+		}
+		result.colums['row'+j] = {'row' :j,'cell':cell};
+  		
+	}
+	console.log(result);
+	$scope.rows = result;
+	
 	$scope.$watch( function(){
- 	var rows = $scope.row;
- 	var row = [];
- 	for(var i=0;i<rows;i++) {
-  		row.push(i+1);
-	}
-	$scope.rows = row;
-	var cells = $scope.cell;
-	var cell = [];
-	for(var i=0;i<cells;i++)
-	{
-		cell.push(i+1);
-	}
- 	$scope.cells=cell;
-    
+ 	
+	
       $scope.MyStyle = {'border':'1px solid #000','height':'20px'};
     });
-    $scope.selectrow = function(e,i)
-    {
-    	console.log(e);
-    	console.log(i);
+    var select = {cell:null,row:null};
+    $scope.selectrow = function($index,row)
+    {	
+    	console.log($index);
+    	console.log(row);
     }
 	 
 }
