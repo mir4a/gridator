3cc93427a799abad98d04ccf4ecfca0bcaa2a953
Artur <coderxlsn@hotmail.com>
2013-09-28 21:02:03 +0300

    #2 создание сетки

--- javascripts/angular/app/controller.js
+++ javascripts/angular/app/controller.js
@@ -1,7 +1,20 @@
 function MyCtrl ($scope) {
- 
-    $scope.$watch( function(){
-      $scope.MyStyle = {'width':$scope.bgsd+'px','border':'1px solid #000','height':$scope.bg+'px'};
+	$scope.$watch( function(){
+ 	var rows = $scope.bgsd;
+ 	var row = [];
+ 	for(var i=0;i<rows;i++) {
+  		row.push(i+1);
+	}
+	$scope.rows = row;
+	var cells = $scope.bg;
+	var cell = [];
+	for(var i=0;i<cells;i++)
+	{
+		cell.push(i+1);
+	}
+ 	$scope.cells=cell;
+    
+      $scope.MyStyle = {'border':'1px solid #000','height':'20px'};
     });
     
 	 
