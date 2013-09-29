97706e4302e3457a67734051a30707eaec7d45b5
Miroslav Martynov <marty.mircha@gmail.com>
2013-09-29 03:13:56 +0300

    #2: merged

--- javascripts/angular/app/controller.js
+++ javascripts/angular/app/controller.js
@@ -1,69 +1,71 @@
-function MyCtrl ($scope) {
+function MyCtrl($scope) {
 
-	$scope.row = 3;
-	$scope.cell = 12;
-	var rows = $scope.row;   //СТРОКИ
- 	var cells = $scope.cell; //КОЛОНКИ
- 	var result = new Object;
- 	result.colums = new Object;
- 	for(var j=0;j<rows;j++) {
- 		var cell =new Object;
- 		for(var i=0;i<cells;i++)
-		{
-			cell['elemnt'+i] = {'cell':1,'check':0,'class1':'not-checked','index':i};
-		}
-		result.colums['row'+j] = {'row' :j,'cell':cell};
-  		
-	}
-	console.log(result);
-	$scope.rows = result;
-	
-	$scope.$watch( 'rows', function(){
- 	
-	
-      $scope.MyStyle = {'border':'1px solid #000','height':'20px'};
-    });
-    var select = new Object;
-    $scope.selectrow = function(cell,row)
-    {	
-    	
-    	 var active = $scope.rows.colums['row'+row].cell['elemnt'+cell];
-    	 if(active.check == 0)
-    	 {
-    	 	active.class1 = 'checked';
-    	 	active.check = 1;
-    	 	$scope.select = 1;
-    	 }else{
-    	 	active.class1 = 'not-checked';
-    	 	active.check = 0;
-    	 }
-    	  if (select.row == null) {
-    	 	select.row = row;
-    	 	select.startcell = cell;
-    	 }else {
-    	 	active.cell++;
-    	 	delete $scope.rows.colums['row'+row].cell['elemnt'+cell+1];
-    	 }
-    	 select[active.$$hashKey]=1;
-    	 /*
-    	 if (select.row == null) {
-    	 	select.row = row;
-    	 	select.startcell = cell;
-    	 }else {
-    	 	if (select.startcell > cell) {
-    	 		select.colums = select.colums+( select.startcell-cell);
-    	 		select.startcell
-    	 	}else {
-    	 		select.colums = select.colums+( cell- select.startcell);
-    	 	}
-    	 }
-    	 */
-    	 console.log(select);
-    	
+  $scope.row = 3;
+  $scope.cell = 12;
+  var rows = $scope.row;   //СТРОКИ
+  var cells = $scope.cell; //КОЛОНКИ
+  var result = new Object;
+  result.colums = new Object;
+  for (var j = 0; j < rows; j++) {
+    var cell = new Object;
+    for (var i = 0; i < cells; i++) {
+      cell['elemnt' + i] = {'cell': 1, 'check': 0, 'class1': 'not-checked', 'index': i};
     }
-    $scope.resuldata = function()
-    {
+    result.colums['row' + j] = {'row': j, 'cell': cell};
 
+  }
+  console.log(result);
+  $scope.rows = result;
+
+  $scope.$watch('rows', function () {
+
+
+    $scope.MyStyle = {'border': '1px solid #000', 'height': '20px'};
+  });
+  var select = new Object;
+  $scope.selectrow = function (cell, row) {
+
+    var active = $scope.rows.colums['row' + row].cell['elemnt' + cell];
+    if (active.check == 0) {
+      active.class1 = 'checked';
+      active.check = 1;
+      $scope.select = 1;
+    } else {
+      active.class1 = 'not-checked';
+      active.check = 0;
     }
-	 
+    if (select.row == null) {
+      select.row = row;
+      select.startcell = cell;
+    } else {
+      active.cell++;
+      delete $scope.rows.colums['row' + row].cell['elemnt' + cell + 1];
+    }
+    select[active.$$hashKey] = 1;
+    if (select.row == null) {
+      select.row = row;
+      select.startcell = cell;
+      select.colums = 1;
+    } else {
+      select.colums = 0;
+      if (select.row != row) {
+        console.log('row' + row);
+        console.log('select.startcell ' + select.startcell);
+        console.log('cell ' + cell);
+        console.log('select.colums ' + select.colums);
+      }
+      if (select.startcell > cell) {
+        select.colums = select.colums + ( select.startcell - cell);
+        select.startcell
+      } else {
+        select.colums = select.colums + ( cell - select.startcell);
+      }
+    }
+    console.log(select);
+
+  }
+  $scope.resuldata = function () {
+
+  }
+
 }
